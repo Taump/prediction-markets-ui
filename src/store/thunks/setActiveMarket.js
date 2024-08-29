19619eabab8444afcff8fb208b85d25faa6f86cf
Taump <taump@yandex.ru>
2024-08-29 18:09:46 +0400

    feat: add venue information to MarketPage

--- src/store/thunks/setActiveMarket.js
+++ src/store/thunks/setActiveMarket.js
@@ -88,6 +88,7 @@ export const setActiveMarket = createAsyncThunk(
     let currencyCurrentValue = 0;
     let league_emblem = null;
     let league = null;
+    let venue = null;
 
     let yes_odds = null;
     let no_odds = null;
@@ -115,8 +116,14 @@ export const setActiveMarket = createAsyncThunk(
       }
 
       const [championship, yes_abbreviation, no_abbreviation] = params.feed_name.split("_");
-      const championships = await backend.getChampionships();
 
+      const [venueData, championships] = await Promise.all([
+        backend.getVenue(params.feed_name).then(data => data?.data).catch(() => null),
+        backend.getChampionships()
+      ]);
+
+      venue = venueData;
+      
       const sport = Object.entries(championships).find(([_, cs]) => cs.find(({ code }) => code === championship));
 
       if (sport) {
@@ -183,6 +190,7 @@ export const setActiveMarket = createAsyncThunk(
 
     return {
       params,
+      venue,
       stateVars,
       base_aa,
       dailyCandles,
